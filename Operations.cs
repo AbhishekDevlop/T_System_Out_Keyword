78beb0207a53e82b7be4fb9341e3756ed2ee4e17
AbhishekDevlop <101594544+AbhishekDevlop@users.noreply.github.com>
2022-04-03 10:47:23 +0530

    Add files via upload
    out keyword

---
+++ Operations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out_Keyword
+{
+    internal class Operations
+    {
+        public void Calculation(int n1, int n2,out int add, out int mul,out int sub) 
+        {
+            add = n1 + n2;
+            sub = n1 - n2;
+            mul = n1 * n2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out_Keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 30;
+            int b = 10;
+            int add, mul, sub;
+
+            Operations op = new Operations();
+            op.Calculation(a,b,out add,out mul,out sub);
+            Console.WriteLine($"{a} + {b} = {add}");
+            Console.WriteLine($"{a} - {b} = {sub}");
+            Console.WriteLine($"{a} * {b} = {mul}");
+        }
+    }
+}
